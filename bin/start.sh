e8500437b1c2fc602e57638fe8982ba0390c043a
zh9314 <h.zhou@uva.nl>
2017-01-24 10:49:25 +0100

    make the docker run ShFileClean.jar when running

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,5 @@
 bash /root/apache-tomcat-6.0.45/bin/startup.sh
-#java -jar /root/SWITCH/bin/ShFileClean.jar /root/SWITCH/bin/ &
+java -jar /root/SWITCH/bin/ShFileClean.jar /root/SWITCH/bin/ &
 while true
 do
   sleep 10s
